40bb6f64cd41e6cfd706cf3c7ed2fc60b7fdfeac
eduargodoy0 <eduargodoy22@gmail.com>
2025-09-07 11:51:47 -0300

    fix: ajustado app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,34 @@
-import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Cadastro from './cadastro';
+import Login from './login';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 
 function App() {
-  const [mensagem, setMensagem] = useState('');
-
-  useEffect(() => {
-    fetch('http://localhost:5000/teste')
-      .then(res => res.json())
-      .then(data => setMensagem(data.mensagem));
-  }, []);
-
-  return <h1>{mensagem}</h1>;
+  return (
+    <Router>
+      <Navbar bg="primary" variant="dark" expand="lg" className="mb-4">
+        <Container>
+          <Navbar.Brand as={Link} to="/">Aplicação de Cadastro</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+            <Nav.Link as={Link} to="/cadastro">Cadastro</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Container>
+        <Routes>
+          <Route path="/" element={
+            <div className="text-center mt-5">
+              <h1>Bem-vindo à Aplicação de Cadastro</h1>
+              <p>Utilize o menu acima para acessar o Login ou Cadastro.</p>
+            </div>
+          } />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+        </Routes>
+      </Container>
+    </Router>
+  );
 }
 
 export default App;
